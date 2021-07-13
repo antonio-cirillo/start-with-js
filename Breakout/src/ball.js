d411a42fb6ff0d3aeaafceac27e8ed5be3e1b809
antonio-cirillo <dev.cirillo@gmail.com>
2021-07-13 21:17:54 +0200

    Create classe Ball

---
+++ Breakout/src/ball.js
@@ -0,0 +1,43 @@
+export default class Ball {
+    
+    constructor(game) {
+        this.image = document.getElementById("ball");
+        this.gameWidth = game.gameWidth;
+        this.gameHeight = game.gameHeight;
+
+        this.game = game;
+
+        this.size = 16;
+        this.position = {
+            x: 10,
+            y: 10
+        };
+        this.speed = {
+            x: 2, 
+            y: 2
+        };        
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.size, this.size);
+    }
+
+    update(deltaTime) {
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+
+        if(this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+            this.speed.x = -this.speed.x;
+        } if(this.position.y + this.size > this.gameHeight || this.position.y < 0) {
+            this.speed.y = -this.speed.y
+        }
+
+        let bottomOfBall = this.size + this.position.y;
+        let topOfPaddle = this.game.paddle.position.y;
+
+        if(bottomOfBall >= topOfPaddle) {
+            this.speed.y = -this.speed.y;
+        }
+    }
+
+}
