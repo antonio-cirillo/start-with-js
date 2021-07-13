b5546c2de547f97d31058663ebcde157f7e56407
antonio-cirillo <dev.cirillo@gmail.com>
2021-07-13 19:18:19 +0200

    Implementato il gameLoop()

--- Breakout/src/main.js
+++ Breakout/src/main.js
@@ -1,4 +1,5 @@
 import Paddle from './paddle.js'
+import InputHandler from './input.js'
 
 let canvas = document.getElementById("gameCanvas");
 
@@ -7,7 +8,22 @@ let ctx = canvas.getContext('2d');
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let inputHandler = new InputHandler(paddle);
+
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop(0);
