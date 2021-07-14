5502b77947eb6741b382a8b8d661da9fb2a0e4ab
antonio-cirillo <dev.cirillo@gmail.com>
2021-07-14 17:20:59 +0200

    Inserita la rimozione dei blocchi colpiti

--- Breakout/src/game.js
+++ Breakout/src/game.js
@@ -1,21 +1,24 @@
 import Paddle from './paddle.js'
 import InputHandler from './input.js'
 import Ball from './ball.js'
+import Brick from './brick.js'
+import { buildLevel, level1 } from './levels.js'
 
 export default class Game {
 
     constructor(gameWidth, gameHeight) {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-    }
-    
+    }    
 
     start() {
         this.paddle = new Paddle(this);
         this.inputHandler = new InputHandler(this.paddle);
         this.ball = new Ball(this);
+        
+        let bricks = buildLevel(this, level1);
 
-        this.gameObjects = [this.ball, this.paddle];
+        this.gameObjects = [this.ball, this.paddle, ...bricks];
     }
 
     draw(ctx) {
@@ -24,7 +27,8 @@ export default class Game {
 
     update(deltaTime) {
         this.gameObjects.forEach((object) => object.update(deltaTime));
-    }
 
+        this.gameObjects = this.gameObjects.filter((object) => !object.markedForDeletion);
+    }
 
 }
